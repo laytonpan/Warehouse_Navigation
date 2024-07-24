779fe524ecd920e3a80ea5c4889952446dfc77e8
layto <laytonpan@gmail.com>
2024-07-24 12:08:01 +1000

    adding test gradle implement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-core:3.9.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation ("androidx.room:room-testing:2.6.1")
+
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
